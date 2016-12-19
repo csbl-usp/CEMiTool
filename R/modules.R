a7487c1e3748a32b3b0dd8708e6de3d93229ae72
Lucas Cardozo <lucasecardozo@gmail.com>
2016-12-19 18:32:08 -0200

    adding verbose parameter to find_modules function

--- R/modules.R
+++ R/modules.R
@@ -4,6 +4,7 @@
 #'
 #' @param exprs gene expression \code{"data.frame"}
 #' @param cor.method a character string indicating which correlation coefficient is to be computed
+#' @param verbose logical. Report analysis steps
 #'
 #' @return just god knows 
 #'
@@ -11,7 +12,9 @@
 #' coex_mods <- find_modules(exprs=expression.df, cor_method='pearson')
 #'
 #' @export
-find_modules <- function(exprs, cor_method=c('pearson', 'spearman')) {
+find_modules <- function(exprs, cor_method=c('pearson', 'spearman'), 
+                         verbose=F)
+{
     #goWGCNA + SplitModules
 }
 
