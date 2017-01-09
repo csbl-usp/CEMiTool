f0d85b02667761e7d76ee1fdbeb342ddfd6552e8
matheuscburger <matheus.cburger@gmail.com>
2017-01-09 21:25:07 -0200

    ORA included

--- R/enrichment.R
+++ R/enrichment.R
@@ -1,16 +1,101 @@
+suppressPackageStartupMessages({
+	    require("fgsea")
+})
+
+
+#' Reads a GMT file
+#'
+#' @param fname GMT file name 
+#'
+#' @return a list containing genes and description of each pathway 
+#'
+#' @examples
+#'
+read_gmt <- function(fname){
+    res <- list(genes=list(), desc=list())
+    gmt <- file(fname)
+    gmt_lines <- readLines(gmt)
+    close(gmt)
+    gmt_list <- lapply(gmt_lines, function(x) unlist(strsplit(x, split="\t")))
+    gmt_names <- sapply(gmt_list, '[', 1)
+    gmt_desc <- lapply(gmt_list, '[', 2)
+    gmt_genes <- lapply(gmt_list, function(x){x[3:length(x)]})
+    names(gmt_desc) <- names(gmt_genes) <- gmt_names
+    return(list(genes=gmt_genes, desc=gmt_desc))
+}
+
+
+#' Transforms a GMT list into inputs for enricher
+#'
+#' @param gmt a GMT list from read.gmt function
+#'
+#' @return a list containing term2gene and term2name
+#'
+#' @examples
+#'
+prepare_gmt <- function(gmt){
+	res <- list()
+	res[["term2gene"]] <- do.call(rbind, lapply(names(gmt[["genes"]]), 
+												function(n) cbind(Term=n, Gene=gmt[["genes"]][[n]])))
+	res[["term2name"]] <- do.call(rbind, lapply(names(gmt[["desc"]]), 
+												function(n) cbind(Term=n, Name=gmt[["desc"]][[n]])))
+	return(res)
+}
+
+#' Performs Over Representation Analysis for a list of genes and a GMT
+#'
+#' @param topgenes a vector of genes
+#' @param gmt.list a gmt from prepare.gmt function
+#' @param allgenes a vector containing all genes to be considered as universe
+#'
+#' @return a data.frame containing the results
+#'
+#' @examples
+#'
+ora <- function(topgenes, gmt_list, allgenes){
+	if(missing(allgenes)){
+		message("Using all genes in GMT file as universe.")
+		allgenes <- unique(gmt_list[["term2gene"]][, "Gene"])
+	}
+	enriched <- enricher(gene = topgenes,
+						 pvalueCutoff = 1,
+						 qvalueCutoff = 1,
+						 universe = allgenes,
+						 TERM2GENE = gmt_list[['term2gene']],
+						 TERM2NAME = gmt_list[['term2name']])
+	result <- enriched@result
+	return(result)
+}
+
+
 #' Module Overrepresentation Analysis
 #'
 #' Perfoms overrepresentation analysis for each co-expression module found
 #'
-#' @param test
+#' @param gene_module 
+#' @param  
 #'
 #' @return None
 #'
 #' @examples
-#' mod_ora(test)
+#' mod_ora(gene_module, gmt)
 #'
 #' @export
-mod_ora <- function(test){}
+mod_ora <- function(gene_module, gmt, verbose=FALSE){
+	gmt_list <- read_gmt(gmt)
+	gmt_in <- prepare_gmt(gmt_list)
+	message("Using all genes in GMT file as universe.")
+	allgenes <- unique(gmt_in[["term2gene"]][, "Gene"])
+	mods <- split(gene_module[, "genes"], gene_module[, "modules"])
+    res_list <- lapply(mods, ora, gmt_in, allgenes)
+	names(res_list) <- names(mods)
+	for(n in names(res_list)){
+		res_list[[n]] <- cbind.data.frame(Module=n, res_list[[n]])
+	}
+	res <- do.call(rbind, res_list)
+	rownames(res) <- NULL
+	return(res)
+}
 
 
 #' Module Gene Set Enrichment Analysis 
