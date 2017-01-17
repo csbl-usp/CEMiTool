6299cc9971878039ac2a31dab078b6c9da0fe1fe
Lucas Cardozo <lucasecardozo@gmail.com>
2017-01-17 17:38:21 -0200

    defining s4 cemitool class

--- R/cemitool.R
+++ R/cemitool.R
@@ -1,3 +1,13 @@
+# creates a CEMiTool object
+setClass('CEMiTool', slots=list(expression='data.frame',
+                                sample_annotation='data.frame',
+                                module='data.frame',
+                                enrichment='list', # gene set enrichment analysis
+                                ora='data.frame',
+                                profile_plot='list',
+                                enrichment_plot='list',
+                                barplot_ora='list'))
+
 #' Full gene co-expression analysis
 #'
 #' Defines co-expression modules and functionally characterizes
@@ -21,7 +31,8 @@
 #' @return a cemitool object 
 #'
 #' @examples
-#' cemitool(exprs=expression.df)
+#' data(exprs) 
+#' cemitool(exprs=exprs)
 #'
 #' @export
 cemitool <- function(exprs, 
@@ -36,16 +47,8 @@ cemitool <- function(exprs,
                      plot=FALSE,
                      verbose=FALSE)
 {
-    # creates a CEMiTool object
-    results <- structure(list(module=NULL,
-                              enrichment=NULL, # gene set enrichment analysis
-                              ora=NULL,
-                              profile_plot=NULL,
-                              enrichment_plot=NULL,
-                              barplot_ora=NULL
-                              ), class = "cemitool")
-
-    results$module <- find_modules(exprs,
+    results <- new('CEMiTool', expression=exprs)
+    results@module <- find_modules(results@expression,
                                    cor_method=match.arg(cor_method),
                                    min_ngen=min_ngen,
                                    merge_similar=merge_similar,
@@ -54,34 +57,33 @@ cemitool <- function(exprs,
 
     # if user wants splitted modules
     if (split_modules) {
-        results$module <- split_modules(exprs=exprs, gene_module=results$module,
-                                        min_ngen=min_ngen,
-                                        verbose=verbose)
+        results <- split_modules(results, min_ngen=min_ngen,
+                                 verbose=verbose)
     }
 
     # if user provides annot file
-    if (!is.null(annot)) {
+    if (!missing(annot)) {
+        results@sample_annotation <- annot
         #run mod_gsea
-        results$enrichment <- mod_gsea(exprs=exprs, gene_module=results$module,
-                                       annot=annot, verbose=verbose)
+        results <- mod_gsea(results, verbose=verbose)
     }
 
     # if user provides .gmt file
-    if (!is.null(gmt)) {
+    if (!missing(gmt)) {
         #run mod_ora
-        results$ora <- mod_ora(gene_module=results$module, gmt=gmt, verbose=verbose)
+        results <- mod_ora(results, gmt=gmt, verbose=verbose)
     }
 
     # plots all desired charts
     if (plot) {
-        results$profile_plot <- plot_profile(exprs, results$module)
+        results <- plot_profile(results)
 
-        if (!is.null(results$enrichment)) {
-            results$enrichment_plot <- plot_gsea(results$enrichment)
+        if (!is.null(results@enrichment)) {
+            results <- plot_gsea(results)
         }
 
-        if (!is.null(results$ora)) {
-            results$barplot_ora <- plot_ora(results$ora)
+        if (!is.null(results@ora)) {
+            results <- plot_ora(results)
         }
     }
     return(results)
@@ -106,8 +108,8 @@ number_modules <- function(x) UseMethod("number_modules")
 #' @export
 number_modules.cemitool <- function(x) {
     n <- 0
-    if(!is.null(res$module)){
-        n <- length(unique(res$module$modules))
+    if(!is.null(res@module)){
+        n <- length(unique(res@module$modules))
     } else {
         message("Run cemitool function to get modules!") 
     }
@@ -125,45 +127,45 @@ print.cemitool <- function(x) {
     cat("CEMiTool Object\n")
     cat("- Number of modules:", number_modules(x), "\n")
     cat("- Modules: ")
-    if(is.null(x$module)){
+    if(is.null(x@module)){
         cat("null\n")
     } else {
-        cat("\b\b (data.frame: ", nrow(x$module), "x", ncol(x$module), "): \n", sep="")
-        print(x$module[1:3, ])
+        cat("\b\b (data.frame: ", nrow(x@module), "x", ncol(x@module), "): \n", sep="")
+        print(x@module[1:3, ])
     }
     cat("- Gene Set Enrichment Analysis: ")
-    if(is.null(x$enrichment)) {
+    if(is.null(x@enrichment)) {
         cat("null\n")
     } else {
         cat("\n    List containing 3 data.frames:\n")
         cat("        - $ es   : Enrichment Scores ")
-        cat("(", nrow(x$enrichment$es), "x", ncol(x$enrichment$es), ") \n", sep="")
+        cat("(", nrow(x@enrichment$es), "x", ncol(x@enrichment$es), ") \n", sep="")
         cat("        - $ nes  : Normalized Enrichment Scores ")
-        cat("(", nrow(x$enrichment$nes), "x", ncol(x$enrichment$nes), ") \n", sep="")
+        cat("(", nrow(x@enrichment$nes), "x", ncol(x@enrichment$nes), ") \n", sep="")
         cat("        - $ pval : p-value ")
-        cat("(", nrow(x$enrichment$pval), "x", ncol(x$enrichment$pval), ") \n", sep="")
+        cat("(", nrow(x@enrichment$pval), "x", ncol(x@enrichment$pval), ") \n", sep="")
     }
     cat("- Over Representation Analysis: ")
-    if(is.null(x$ora)) {
+    if(is.null(x@ora)) {
         cat("null\n")
     } else {
-        cat("\b\b (data.frame: ", nrow(x$ora), "x", ncol(x$ora), "): \n", sep="")
-        print(x$ora[1:3, c('Module', "ID", "p.adjust")])
+        cat("\b\b (data.frame: ", nrow(x@ora), "x", ncol(x@ora), "): \n", sep="")
+        print(x@ora[1:3, c('Module', "ID", "p.adjust")])
     }
     cat("- Profile plot: ")
-    if(is.null(x$profile_plot)) {
+    if(is.null(x@profile_plot)) {
         cat("null\n")
     } else {
         cat("ok\n")
     }
     cat("- Enrichment plot: ")
-    if(is.null(x$enrichment_plot)) {
+    if(is.null(x@enrichment_plot)) {
         cat("null\n")
     } else {
         cat("ok\n")
     }
     cat("- Barplot of ORA: ")
-    if(is.null(x$barplot_ora)) {
+    if(is.null(x@barplot_ora)) {
         cat("null\n")
     } else {
         cat("ok\n")
