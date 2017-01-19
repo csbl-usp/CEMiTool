57a3bd496b89bca6eeb6685ec1a03e1a27951ee4
Lucas Cardozo <lucasecardozo@gmail.com>
2017-01-19 17:51:30 -0200

    creating integrate.R file

---
+++ R/integrate.R
@@ -0,0 +1,14 @@
+#' importFrom gRbase combnPrim
+
+#' Integrates CEMiTooli
+#'
+#' 
+#'
+#' @param analyses List of objects of class \code{CEMiTool}
+#'
+#' @export
+integrate <- function(analyses) {
+    edgelist <- lapply(analyses, function(cem) {
+        
+    })
+}
