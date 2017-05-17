0eb9f81ce12d009ad9c4b8bb6388935b457d6330
pedrostrusso <pedrostrusso@gmail.com>
2017-05-17 11:19:00 -0300

    Adding expression filter to filter
    Adding a filter that selects top 75% most expressed genes before applying the variance filters

--- R/filter.R
+++ R/filter.R
@@ -23,74 +23,85 @@ setGeneric('filter_expr', function(cem, ...) {
 #' @rdname filter_expr
 #' @export
 setMethod('filter_expr', signature('CEMiTool'),
-          function(cem, pval=0.1, n_genes, dtype=c('microarray', 'rnaseq'))
-{
-    if(!missing(n_genes)){
-        if(!missing(pval)){
-            stop("Please specify exclusively pval or n_genes.")
-        }
-    }
-    expr <- expr_data(cem, filtered=FALSE)
+        function(cem, pval=0.1, n_genes, pct=0.75, dtype=c('microarray', 'rnaseq')){
+              if(!missing(n_genes)){
+                  if(!missing(pval)){
+                      stop("Please specify exclusively pval or n_genes.")
+                  }
+              }
+              expr <- expr_data(cem, filtered=FALSE)
               
-    expr_var <- apply(expr, 1, var)
+              expr <- expr_pct_filter(expr, pct)
               
-    expr <- expr[which(expr_var!=0),]
+              expr_var <- apply(expr, 1, var)
               
-    if(!missing(n_genes)){
-        n_genes <- min(n_genes, nrow(expr))
-    }
-
-    dtype <- match.arg(dtype)
-    
-    if (dtype == 'rnaseq') {
-        expr <- vst(expr)
-    }
-
-    expr_var <- apply(expr, 1, var)
-
-    mean_var <- mean(expr_var)
-    var_var <- var(expr_var)
-
-    ah <- mean_var^2/var_var + 2
-    bh <- mean_var*(ah - 1)
-
-    p <- sapply(expr_var, function(x) {
-        ig <- pracma::gammainc(bh/x, ah)['uppinc']
-        g <- gamma(ah)
-        return(1 - ig/g)
-    })
-
-    names(p) <- gsub('.uppinc', '', names(p))
-
-    if(!missing(n_genes)){
-        pval <- sort(p)[n_genes]
-        names(pval) <- NULL
-    }
-
-    selected <- which(p <= pval)
-    
-    if (length(selected) > 0) {
-        cem@selected_genes <- names(selected)
-    } else {
-        cem@selected_genes <- selected
-        warning('No gene left after the filtering')
-    }
-
-    cem@parameters <- c(cem@parameters, 
-                            n_genes=length(selected),
-                            filter_pval=pval)
-
-    return(cem)
-})
+              expr <- expr[which(expr_var!=0),]
+              
+              if(!missing(n_genes)){
+                  n_genes <- min(n_genes, nrow(expr))
+              }
+              
+              dtype <- match.arg(dtype)
+              
+              if (dtype == 'rnaseq') {
+                  message("New filter")
+                  expr <- vst(expr)
+                  expr_data(cem) <- expr
+              }
+              
+              expr_var <- apply(expr, 1, var)
+              
+              mean_var <- mean(expr_var)
+              var_var <- var(expr_var)
+              
+              ah <- mean_var^2/var_var + 2
+              bh <- mean_var*(ah - 1)
+              
+              p <- sapply(expr_var, function(x) {
+                  ig <- pracma::gammainc(bh/x, ah)['uppinc']
+                  g <- gamma(ah)
+                  return(1 - ig/g)
+              })
+              
+              names(p) <- gsub('.uppinc', '', names(p))
+              
+              if(!missing(n_genes)){
+                  pval <- sort(p)[n_genes]
+                  names(pval) <- NULL
+              }
+              
+              selected <- which(p <= pval)
+              
+              if (length(selected) > 0) {
+                  cem@selected_genes <- names(selected)
+              } else {
+                  cem@selected_genes <- selected
+                  warning('No gene left after the filtering')
+              }
+              
+              cem@parameters <- c(cem@parameters, 
+                                  n_genes=length(selected),
+                                  filter_pval=pval)
+              
+              return(cem)
+          })
 
 vst <- function(expr) {
     gene_mean <- apply(expr, 1, mean)
     gene_var  <- apply(expr, 1, var)
-
+    
     if(cor(gene_mean, gene_var, method="spearman") > 0.5){
         r <- sum(gene_mean^4)/(sum(gene_var*(gene_mean^2)) - sum(gene_mean^3))
         return(sqrt(r)*asinh(sqrt(expr/r)))
     }else{
         return(expr)
     }
+}
+
+expr_pct_filter <- function(expr, pct=0.75){
+    rows <- floor(pct*nrow(expr))
+    val <- apply(expr, 1, mean)
+    sel_rows <- order(val, decreasing=TRUE)[1:rows]
+    expr <- expr[sel_rows, ]
+    return(expr)
 }
