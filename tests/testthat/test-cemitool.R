6daf1da1966e2571b462b5a513262dc1fb1a98f6
pedrostrusso <pedrostrusso@gmail.com>
2017-06-26 16:37:23 -0300

    Updating tests

--- tests/testthat/test-cemitool.R
+++ tests/testthat/test-cemitool.R
@@ -24,7 +24,7 @@ test_that('all methods of signature CEMiTool returns CEMiTool objects', {
     
     expect_equal(class(cem_fm)[1], 'CEMiTool') 
    
-    cem <- include_interactions(cem_fm, ppi)
+    interactions_data(cem) <- ppi
     expect_equal(class(cem)[1], 'CEMiTool') 
 
     cem <- mod_gsea(cem)
