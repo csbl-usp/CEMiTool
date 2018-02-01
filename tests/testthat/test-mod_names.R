2d6e555a703ae27e62131bbd214c892373d60f6d
pedrostrusso <pedrostrusso@gmail.com>
2018-02-01 12:49:21 -0200

    fixing testthat bug

--- tests/testthat/test-mod_names.R
+++ tests/testthat/test-mod_names.R
@@ -8,4 +8,4 @@ test_that('a warning is thrown if there are no modules', {
 test_that('modules are sorted by size', {
     data(cem)
     expect_true(identical(mod_names(cem), names(sort(table(cem@module$modules), decreasing=T))))
-}
+})
