ef1c4a24a0cb8de4a2102db1a63b8bba44737137
Lucas Cardozo <lucasecardozo@gmail.com>
2018-11-23 14:04:31 -0200

    fixing write_files for pval->padj change

--- tests/testthat/test-cemitool.R
+++ tests/testthat/test-cemitool.R
@@ -19,6 +19,8 @@ gmt <- read_gmt(gmt)
 
 cem_filt <- filter_expr(cem_base, 1)
 
+cem_full <- cem
+
 test_that('all methods of signature CEMiTool returns CEMiTool objects', {
     expect_is(cem_filt, 'CEMiTool') 
     
@@ -47,6 +49,8 @@ test_that('all methods of signature CEMiTool returns CEMiTool objects', {
 
     cem <- plot_mean_k(cem)
     expect_is(cem, 'CEMiTool') 
+
+    cem_full <<- cem
 })
 
 test_that('CEMiTool throws errors when there is missing data', {
@@ -58,4 +62,12 @@ test_that('CEMiTool throws errors when there is missing data', {
     expect_warning(mod_ora(cem0, gmt), "No modules in CEMiTool object! Did you run find_modules()?")
 })
 
-
+test_that('Write files work as expected', {
+    outputs <- c("enrichment_es.tsv", "enrichment_nes.tsv", "enrichment_padj.tsv",
+                 "interactions.tsv", "module.tsv", "modules_genes.gmt",
+                 "ora.tsv", "parameters.tsv", "selected_genes.txt",
+                 "summary_eigengene.tsv", "summary_mean.tsv", "summary_median.tsv")
+    expect_error(write_files(cem_full, force=TRUE), NA)
+    expect_equal(outputs, dir('Tables'))
+    unlink('Tables', recursive=TRUE)
+})
